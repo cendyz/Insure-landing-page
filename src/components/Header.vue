4fc0a449b4df7ecff80c9059eaa2cada428401bb
cendyz <arek57823@gmail.com>
2025-01-27 09:39:22 +0100

    header desktop

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,60 @@
 <template>
-	<header>
-		<img :src="mobilePattern" alt="dad, mom and two kids" />
+	<header class="bg-darkViolet lg:py-[4em]">
+		<img
+			:src="mobilePattern"
+			alt="dad, mom and two kids"
+			class="block w-full lg:hidden" />
+
 		<div
-			class="bg-darkViolet text-center py-[5em] px-[1.7em] text-veryLightGray relative">
-			<h1 class="text-[4.5rem] font-display font-display400 z-10 relative">
-				Humanizing your insurance.
-			</h1>
-			<p class="mt-[1em] leading-[1.8] z-10 relative">
-				Get your life insurance coverage easier and faster. We blend our
-				expertise and technology to help you find the plan that’s right for
-				you. Ensure you and your loved ones are protected.
-			</p>
-			<button
-				type="button"
-				class="uppercase mt-[2em] py-[.4em] px-[1.7em] border-2 text-[1.9rem] relative">
-				view plans
-			</button>
-			<img :src="topLeftLines" alt="" class="absolute top-0 left-0 z-0" />
-			<img
-				:src="bottomRightLines"
-				alt=""
-				class="absolute bottom-[-38%] right-0 z-0" />
+			class="py-[5em] px-[1.7em] text-veryLightGray relative flex w-[100%] lg:justify-between lg:container">
+			<div class="text-center lg:w-[44%] lg:text-left">
+				<div
+					class="hidden lg:block h-[2px] rounded-2xl w-[40%] bg-veryLightGray"></div>
+				<h1
+					class="text-[4.5rem] font-display font-display400 z-10 relative lg:text-[7.3rem] lg:mt-[.8em]">
+					Humanizing your insurance.
+				</h1>
+				<p class="mt-[1em] leading-[1.8] z-10 relative lg:text-[1.6rem]">
+					Get your life insurance coverage easier and faster. We blend our
+					expertise and technology to help you find the plan that’s right
+					for you. Ensure you and your loved ones are protected.
+				</p>
+				<button
+					type="button"
+					class="uppercase mt-[2em] py-[.4em] px-[1.7em] border-2 text-[1.9rem] relative">
+					view plans
+				</button>
+			</div>
+			<picture>
+				<source :srcset="desktopPattern" media="(min-width: 992px)" />
+				<img
+					:src="mobilePattern"
+					alt="dad, mom and two kids"
+					class="hidden w-full lg:block lg:absolute lg:right-[1.5em] lg:w-[52%] z-10" />
+			</picture>
+			<picture>
+				<source :srcset="desktopTopLines" media="(min-width: 992px)" />
+				<img
+					:src="topLeftLines"
+					alt=""
+					class="absolute top-0 left-0 z-0 lg:left-full lg:-translate-x-1/2 lg:top-[-10%]" />
+			</picture>
+			<picture>
+				<source :srcset="desktopBotLines" media="(min-width: 992px)" />
+				<img
+					:src="bottomRightLines"
+					alt=""
+					class="absolute bottom-[-38%] right-0 z-0 lg:right-full lg:bottom-[-55%]" />
+			</picture>
 		</div>
 	</header>
 </template>
 
 <script setup lang="ts">
 import mobilePattern from '@/images/image-intro-mobile.jpg'
+import desktopPattern from '@/images/image-intro-desktop.jpg'
 import topLeftLines from '@/images/bg-pattern-intro-left-mobile.svg'
 import bottomRightLines from '@/images/bg-pattern-intro-right-mobile.svg'
+import desktopTopLines from '@/images/bg-pattern-intro-right-desktop.svg'
+import desktopBotLines from '@/images/bg-pattern-intro-left-desktop.svg'
 </script>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,15 @@
 <template>
 	<nav
-		class="fixed top-0 left-0  w-full flex justify-between p-[1.5em] items-center bg-veryLightGray z-[100]">
+		class="fixed top-0 left-0  w-full flex justify-between p-[1.5em] items-center bg-veryLightGray z-[100] lg:static lg:container">
 		<img :src="logo" alt="logo workit" class="block" />
-		<button @click="isOpenNavMenu = !isOpenNavMenu">
+		<button @click="isOpenNavMenu = !isOpenNavMenu" class="lg:hidden">
 			<img :src="burgerOpen" alt="menu open icon" v-if="!isOpenNavMenu" />
 			<img :src="burgerClose" alt="menu close icon" v-else />
 		</button>
 		<Transition name="navMenu">
 			<div
 				v-if="isOpenNavMenu"
-				class="fixed w-full top-[5em] left-0 h-[calc(100%-5em)] bg-veryDarkViolet flex flex-col items-center px-[1.3em] py-[2.2em] gap-y-[.8em]">
+				class="fixed w-full top-[5em] left-0 h-[calc(100%-5em)] bg-veryDarkViolet flex flex-col items-center px-[1.3em] py-[2.2em] gap-y-[.8em] lg:hidden">
 				<img :src="navPattern" alt="" class="absolute bottom-0 w-full" />
 				<a
 					href="#"
@@ -21,6 +21,17 @@
 				>
 			</div>
 		</Transition>
+		<div
+			class="space-x-[2em] hidden lg:flex">
+			<a
+				href="#"
+				class="text-veryGrayishViolet uppercase text-[2rem] last:border-2 py-[.5em]  last:text-center last:px-[1.5em] last:border-veryDarkViolet"
+				v-for="item in mobileMenuLinks"
+				@click="isOpenNavMenu = false"
+				:key="nanoid()"
+				>{{ item.link }}</a
+			>
+		</div>
 	</nav>
 </template>
 
